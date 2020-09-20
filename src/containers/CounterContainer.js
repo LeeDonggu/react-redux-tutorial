09bb7ddd640948491b3f0d4abea16b7f27f4e730
ldg <7sus7@naver.com>
2020-09-20 15:41:46 +0900

    CounterContainer useSelector, useDispatch 적용

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,61 +1,18 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
-import { connect } from 'react-redux';
 import { increase, decrease } from '../modules/counter';
-// import { bindActionCreators } from 'redux';
-// 하단의 주석 connect에서 사용됨
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter
+      number={number}
+      onIncrease={() => dispatch(increase())}
+      onDecrease={() => dispatch(decrease())}
+    />
   );
 };
 
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounterContainer);
-
-// export default connect(
-//   (state) => ({
-//     number: state.counter.number,
-//   }),
-//   (dispatch) =>
-//     bindActionCreators(
-//       {
-//         increase,
-//         decrease,
-//       },
-//       dispatch,
-//     ),
-// )(CounterContainer);
-
-// export default connect(
-//   (state) => ({
-//     number: state.counter.number,
-//   }),
-//   (dispatch) => ({
-//     increase: () => dispatch(increase()),
-//     decrease: () => dispatch(decrease()),
-//   }),
-// )(CounterContainer);
-
-// const mapStateToProps = (state) => ({
-//   number: state.counter.number,
-// });
-// const mapDispatchToProps = (dispatch) => ({
-//   // 임시 함수
-//   increase: () => {
-//     dispatch(increase());
-//   },
-//   decrease: () => {
-//     dispatch(decrease());
-//   },
-// });
-
-// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default CounterContainer;
